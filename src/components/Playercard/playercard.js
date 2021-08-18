c60cc91638b21ac2b2981fb16900713267e02485
Zak Monnet <zakmonnet@gmail.com>
2021-08-18 14:02:03 -0500

    added ability to update player name

--- src/components/Playercard/playercard.js
+++ src/components/Playercard/playercard.js
@@ -1,16 +1,25 @@
 import React from "react";
 
 function Playercard() {
+  let playerName = "Player 1";
   let phase = 1;
   let score = 0;
 
+  function changeName() {
+    playerName = prompt("What is this player's name?");
+    if (playerName !== "") {
+      document.getElementById("currentPlayerName").innerHTML = playerName;
+    }
+  }
   function advancePhase() {
     phase += 1;
     document.getElementById("currentPhase").innerHTML = phase;
   }
 
   function updateScore() {
-    let points = Number(prompt("How many points are you adding?"));
+    let points = Number(
+      prompt("How many points are you adding to " + playerName + "'s score?")
+    );
     score += points;
     document.getElementById("currentScore").innerHTML = score;
   }
@@ -18,7 +27,13 @@ function Playercard() {
   return (
     <nav class="navbar navbar-light bg-light">
       <div class="container-fluid">
-        <span class="navbar-brand mb-0 h1">Player 1</span>
+        <span
+          id="currentPlayerName"
+          class="navbar-brand mb-0 h1"
+          onClick={changeName}
+        >
+          Player 1
+        </span>
         <button type="button" class="btn btn-primary" onClick={advancePhase}>
           Phase <span id="currentPhase">1</span>
         </button>
